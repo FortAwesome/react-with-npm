6604e91eabd94e246330dd832ee851fb35718713
christianhelms <helmsmedia@gmail.com>
2021-01-16 15:19:55 -0600

    finished example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRocket, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 function App() {
   return (
     <div>
      <p>Hello</p>
+     <FontAwesomeIcon icon={faRocket}/>
+     <FontAwesomeIcon icon={faCoffee}/>
     </div>
   );
 }
